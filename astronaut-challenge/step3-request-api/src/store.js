b687d509fddfe902b47d08b18d83dd1ec005d6d0
Brantschen Patrizio <bpa@ti8m.ch>
2023-10-05 20:49:09 +0200

    Add possible solution for step 3

--- astronaut-challenge/step3-request-api/src/store.js
+++ astronaut-challenge/step3-request-api/src/store.js
@@ -3,12 +3,8 @@ import { writable, derived } from 'svelte/store';
 
 export const apiData = writable([]);
 
-/** Data transformation.
-For our use case, we only care about the drink names, not the other information.
-Here, we'll create a derived store to hold the drink names.
-**/
 export const people = derived(apiData, ($apiData) => {
-  if ($apiData.people){
+  if ($apiData.people) {
     return $apiData.people;
   }
   return [];
